964bc2874283835d1a2153ee86194e70d0066fea
Maximiliano Blanco <inkcoming@protonmail.com>
2020-06-18 15:58:14 -0300

    Add react-reaveal and Fade transition to Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Components
 import Campfire from 'components/Campfire';
+import Fade from 'react-reveal/Fade';
 // Styles
 import './index.scss';
 
@@ -8,10 +9,15 @@ const Home = ({ history }) => (
   <div className="home-campfireBot-container">
     <div className="home-campfireBot-content">
       <Campfire history={history} />
+
       <div className="home-campfireBot-greeting">
-        <h1>¡Hola humano!</h1>
-        <label>¿Querés charlar?</label>
-        <label>Hace click sobre mi para comenzar.</label>
+        <Fade opposite>
+          <h1>¡Hola humano!</h1>
+        </Fade>
+        <Fade opposite>
+          <label>¿Querés charlar?</label>
+          <label>Hace click sobre mi para comenzar.</label>
+        </Fade>
       </div>
     </div>
   </div>
